0357d3964c85921fabf6321449eb0841160c8083
heshara2000 <hesharadananjanee@gmail.com>
2023-10-08 13:26:32 +0530

    add images

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
           <a class="nav-link" href="#">contact</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">about</a>
+          <a class="nav-link" href="#">about</a> 
         </li>
        
         
